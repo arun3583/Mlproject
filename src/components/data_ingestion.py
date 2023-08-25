cc334fd2048e36782242e3a532decc41981cbbe3
arun3583 <arunaswin3583@gmail.com>
2023-08-25 17:08:23 +0530

    best model created

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTranformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path=os.path.join('artifact','train.csv')
@@ -30,6 +32,10 @@ class DataIngestion:
             return(self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)
         except Exception as e:
             raise CustomException(e,sys)
-if __name__=="__main__":
+if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_tranformation=DataTranformation()
+    train_arr,test_arr,_=data_tranformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
